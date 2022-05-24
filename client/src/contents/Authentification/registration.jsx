71a6f65579aab6991cfb14db3c6f2778bbec9237
Omurakunov Abdulaziz <zizomur@yandex.com>
2022-05-24 17:57:37 +0600

    registrated done

--- client/src/contents/Authentification/registration.jsx
+++ client/src/contents/Authentification/registration.jsx
@@ -1,74 +1,68 @@
 import axios from "axios"
 import { useState } from "react"
+import { Navigate } from 'react-router-dom'
+
 
 function Registration(props) {
     const initialValues = {email:"", password:"", password_confirmation:""}
     const [formValues, setFormValues] = useState(initialValues)
-    const [formErrors, setFormErrors] = useState({})
-
+    const [err, setErr] = useState([])
+    const [isSucces, setIsSucces] = useState(false)
     const handleChange = (e) =>{
         const {name, value} = e.target
         setFormValues({...formValues, [name]:value })
     }
-    const validate = (values) =>{
-        const errors = {}
-        const regex = /^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+/g
-        if(!values.email){
-            errors.email = "Email is required"
-        }else if(!regex.test(values.email)){
-            errors.email = "Email is not valid"
-        }
-        if(!values.password){
-            errors.password = "Password is required"
-        }else if (values.password.length < 4){
-            errors.password = "Password length can't be less than 4"
-        }else if (values.password.length > 8){
-            errors.password = "Password length can't be more than 8"
-        }
-        if(!values.password_confirmation){
-            errors.password_confirmation = "Password confirmation is required"
-        }else if ( values.password_confirmation !== values.password ){
-            errors.password = "Password doesn't match"
-        }
-        return errors
-    }
     const registrationReq = () =>{
-        if(Object.keys(formErrors).length === 0){
-            axios
-            .post('http://164.92.91.86/account/register/',{
-                email: formValues.email,
-                password: formValues.password,
-                password_confirmation: formValues.password_confirmation
+        axios
+        .post('http://164.92.91.86/account/register/',{
+            email: formValues.email,
+            password: formValues.password,
+            password_confirmation: formValues.password_confirmation
         })
-        }
+        .then(setIsSucces(true))
+        .catch(err=>{
+            setErr(err)
+            setIsSucces(false)
+        })
+        
+        
     }
-    const handleSubmit = (e) =>{
+    
+    const handleSubmit = async (e) =>{
         e.preventDefault()
-        setFormErrors(validate(formValues))
-        registrationReq(formErrors)
+        setErr([])
+        await registrationReq()
     }
-
+    console.log(isSucces)
     return(
         <div className="page-container">
-            <pre>{JSON.stringify(formValues, undefined , 2)}</pre>
-            <div className="auth-container">
-                <form onSubmit={e=>{handleSubmit(e)}}>
-                    <h1>Registration</h1>
-                    <div className="field">
-                        <label>Email</label>
-                        <input name="email" value={formValues.email} onChange={e=>{handleChange(e)}}></input>
-                    </div>
-                    <div className="field">
-                        <label>Password</label>
-                        <input name="password" value={formValues.password} onChange={e=>{handleChange(e)}}></input>
-                    </div>
-                    <div className="field">
-                        <label>Password confimation</label>
-                        <input name="password_confirmation" value={formValues.password_confirmation} onChange={e=>{handleChange(e)}}></input>
-                    </div>
-                    <button>Submit</button>
-                </form>
-            </div>
+            {
+                isSucces 
+                ? <Navigate replace to="/"></Navigate>
+
+                :<div className="auth-container">
+                    <form onSubmit={e=>{handleSubmit(e)}}>
+                        <h1>Registration</h1>
+                        <div className="field">
+                            <label>Email</label>
+                            <input name="email" value={formValues.email} type="email" onChange={e=>{handleChange(e)}} required></input>
+                            {err.response?.data?.email ? <p>{err.response?.data?.email}</p> : <p> </p>}
+                        </div>
+                        <div className="field">
+                            <label>Password</label>
+                            <input name="password" value={formValues.password} type="password" onChange={e=>{handleChange(e)}} required></input>
+                            {err.response?.data?.password ? <p>{err.response?.data?.password}</p> : err.response?.data?.non_field_errors[0] ?<p>{err.response?.data?.non_field_errors[0]}</p> : <p></p>}
+                        </div>
+                        <div className="field">
+                            <label>Password confimation</label>
+                            <input name="password_confirmation" value={formValues.password_confirmation} type="password" onChange={e=>{handleChange(e)}} required></input>
+                            {err.response?.data?.password_confirmation ? <p>{err.response?.data?.password_confirmation}</p> : err.response?.data?.non_field_errors[0] ?<p>{err.response?.data?.non_field_errors[0]}</p> : <p></p>}
+                        </div>
+                        <button>Submit</button>
+                    </form>
+                </div>
+            }
+            
         </div>
   
     )
--- client/src/contents/navbar.jsx
+++ client/src/contents/navbar.jsx
@@ -15,7 +15,7 @@ function Navbar(){
 
 
     const handleBurger = () =>{
-        setIsOpen(!isOpen)
+        setIsOpen(!isOpen)  
     }
     return(
         <div className="navbar">
